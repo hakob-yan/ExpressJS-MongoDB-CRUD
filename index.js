f62483d6a442cbc1b54d163d5b5ddc6d8e9ff4a9
hakob-yan <hakob9735@gmail.com>
2022-11-14 21:09:43 +0400

    extracting repetitive code

--- index.js
+++ index.js
@@ -1,22 +1,32 @@
 const express = require('express');
 const path = require('path');
+const ejs=require('ejs')
 
 const app = new express();
+app.set('view engine','ejs')
 const options = {};
 app.use(express.static('public/'))
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/index.html'))
+    // res.sendFile(path.resolve(__dirname, 'pages/index.html'))
+    res.render('index')
+
 })
 
 app.get('/about', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/about.html'))
+    // res.sendFile(path.resolve(__dirname, 'pages/about.html'))
+    res.render('about')
+
 })
 app.get('/contact', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/contact.html'))
+    // res.sendFile(path.resolve(__dirname, 'pages/contact.html'))
+    res.render('contact')
+
 })
 app.get('/post', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'pages/post.html'))
+    // res.sendFile(path.resolve(__dirname, 'pages/post.html'))
+    res.render('post')
+
 })
 
 
