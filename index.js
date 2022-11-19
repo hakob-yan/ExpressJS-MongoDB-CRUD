80529b1a13163dd5fb64d014fca8c5dccdbcbed7
hakob-yan <hakob9735@gmail.com>
2022-11-19 23:21:59 +0400

    almost done

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
 const express = require('express');
 const path = require('path');
-const ejs=require('ejs')
+const ejs = require('ejs');
+const { default: mongoose } = require('mongoose');
+const bodyParser = require('body-parser')
+const BlogPost = require('./models/BlogPost');
+
+mongoose.connect('mongodb://localhost/my_database', { useNewUrlParser: true })
 
 const app = new express();
-app.set('view engine','ejs')
+app.set('view engine', 'ejs')
 const options = {};
 app.use(express.static('public/'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
     // res.sendFile(path.resolve(__dirname, 'pages/index.html'))
-    res.render('index')
+    const blogposts = await BlogPost.find({});
+    console.log(blogposts);
+    res.render('index', { blogposts })
 
 })
-
+app.get('/posts/new', (req, res) => {
+    res.render('create')
+})
+app.get('/post/:id', async (req, res) => {
+    const blogpost = await BlogPost.findById(req.params.id);
+    res.render('/post', { blogpost })
+})
 app.get('/about', (req, res) => {
     // res.sendFile(path.resolve(__dirname, 'pages/about.html'))
     res.render('about')
@@ -30,6 +45,11 @@ app.get('/post', (req, res) => {
 })
 
 
+app.post('/posts/store', async (req, res) => {
+    console.log(req.body);
+    await BlogPost.create(req.body);
+    res.redirect('/')
+})
 app.listen(4000, () => {
     console.log('App listenong to 4000 port');
 })
